75b2f812bfbd3c2411501fac4b57b0f280be7497
Samuelj58 <144460795+Samuelj58@users.noreply.github.com>
2024-02-05 10:13:47 -0500

    ww

---
+++ project/src/components/Destcard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>{{ Destination.name }}</h2>
+    </div>
+</template>
+
+<script setup>
+import Destcard from 
+const props = defineProps ({
+    Destination: Object,
+});
+</script>
+
+<style lang="scss" scoped></style>
--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -1,9 +1,10 @@
+<template>
+  <div>
+    <Destcard v-for="" destination in destinations" :key="destination.name" :Destination="destination" </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped></style>
